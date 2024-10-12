f0c43df4aa52628f98b23ee31ce58b64523c4fe0
Scardell <153379926+scardell@users.noreply.github.com>
2024-10-12 19:39:17 +0200

    categorias y aadir vinculos

--- proyectoWeb/urls.py
+++ proyectoWeb/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('servicios/', include('servicios.urls')),
+    path('blog/', include('blog.urls')),
 
     path('', include('ProyectoWebApp.urls')),
 
